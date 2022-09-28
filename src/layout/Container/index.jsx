21fef5866fe9d24397c5a4a4c0790f4dfcf4ccb7
Mustafaev <ubaydullo.mustafaev@mail.ru>
2022-09-28 16:51:39 +0500

    project starting...

---
+++ src/layout/Container/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classNames from "classnames";
+
+import classes from "./Container.module.scss";
+
+const Container = ({ className, children }) => {
+  return (
+    <div className={classNames(classes["container"], className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
